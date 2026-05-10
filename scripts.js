e660e1b1d841caf4054284511bb9fd48eac7af39
medleminlashar44-byte <medleminlashar44@gmail.com>
2026-05-10 00:40:52 +0000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,21 @@
+// 1. Select the elements from your HTML
+const video = document.getElementById('webcam');
+const btn = document.getElementById('cameraBtn');
+
+async function startCamera() {
+    try {
+        
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        
+        
+        video.srcObject = stream;
+        
+        console.log("Camera started successfully!");
+    } catch (err) {
+        
+        alert("Error: Could not access camera. " + err);
+    }
+}
+
+
+btn.addEventListener('click', startCamera);
